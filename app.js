7a721899b387b67b61244712bd8c491d515bd33f
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-10-06 16:48:56 -0700

    CSS, html built to start testing JS

--- app.js
+++ app.js
@@ -3,9 +3,46 @@
 import './auth/user.js';
 
 /* Get DOM Elements */
+const getPost = document.getElementById('post-list');
+const getComment = doument.getElementById('comment-list');
+const errorDisplay = document.getElementById('error-display')
 
 /* State */
+let error = null;
+let post = [];
+let comment = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await getPost();
+    error = response.error;
+    post = response.data;
+
+    if (error) {
+        displayError();
+    }
+
+    if (post) {
+        displayPost();
+    }
+});
 
 /* Display Functions */
+
+function displayError() {
+    if (error) {
+        errorDisplay.textContent = error.message;
+    } else {
+        errorDisplay.textContent = '';
+    }
+}
+
+function displayPost() {
+    postList.innerHTML = '';
+
+    for (const post of posts) {
+        const postEl = renderPost(post);
+        postList.append(postEl);
+    }
+}
+
